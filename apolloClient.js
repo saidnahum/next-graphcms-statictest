9bc2dc47ee81f5dad40b4e959eafdd5e9b3d4657
saidnahum <70162272+saidnahum@users.noreply.github.com>
2022-04-07 17:40:17 -0500

    update

--- apolloClient.js
+++ apolloClient.js
@@ -1,4 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { bootstrap as bootstrapGlobalAgent } from 'global-agent';
+
+bootstrapGlobalAgent();
 
 const client = new ApolloClient({
   uri: "https://api-us-east-1.graphcms.com/v2/ckwqkllh52wgl01xm824qgoly/master",
